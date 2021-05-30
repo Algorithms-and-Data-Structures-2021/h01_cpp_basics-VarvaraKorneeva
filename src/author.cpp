0199137914f158ddaebe63d6bf985c8ed3a697cf
Varvara Korneeva <varvara_korneeva@mail.ru>
2021-05-30 17:36:59 +0300

    h01

--- src/author.cpp
+++ src/author.cpp
@@ -4,7 +4,8 @@
 
 // 1. реализуйте конструктор ...
 Author::Author(const std::string &full_name, int age, Sex sex) {
-  // валидация аргументов (здесь был Рамиль)
+
+    // валидация аргументов (здесь был Рамиль)
   if (age < kMinAuthorAge) {
     throw std::invalid_argument("Author::age must be greater than " + std::to_string(kMinAuthorAge));
   }
@@ -13,6 +14,9 @@ Author::Author(const std::string &full_name, int age, Sex sex) {
     throw std::invalid_argument("Author::full_name must not be empty");
   }
   // Tip 1: инициализируйте поля
+  full_name_ = full_name;
+  age_ = age;
+  sex_ = sex;
 }
 
 void Author::SetAge(int age) {
